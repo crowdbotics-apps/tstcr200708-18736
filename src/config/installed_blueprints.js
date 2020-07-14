9fbc2c2ab062d57072a2d8936fe53a38431558d4
Crowdbotics <team@crowdbotics.com>
2020-07-14 22:24:28 +0000

    add package blueprint-react-native-achievement-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Achievements778530', human_name: 'Achievements 7', access_route: 'Achievements778530'},
 { name: 'Messenger', human_name: 'Messenger', access_route: 'Messenger', icon: 'comments'},
 { name: 'Tutorial', human_name: 'Tutorial', access_route: 'Tutorial', icon: 'question-circle'},
 { name: 'Maps', human_name: 'Maps', access_route: 'Maps', icon: 'map'},
